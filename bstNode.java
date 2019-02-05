75075438861fc514d4294dd3a862459cdf194422
Drew Taylor <drewtaylorm@gmail.com>
2019-02-05 16:05:05 -0500

    first commit

---
+++ bstNode.java
@@ -0,0 +1,13 @@
+public class bstNode
+{
+    public int data;
+    public bstNode left;
+    public bstNode right;
+
+    public bstNode(int newData)
+    {
+        this.data=newData;
+        this.left=null;
+        this.right=null;
+    }
+}
